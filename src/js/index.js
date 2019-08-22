89e6ea4983f72827c6fa08b168a955eafe3a0b74
VEGALUISJ <Luisvega.jlvv@gmail.com>
2019-08-22 00:11:18 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
 
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["The "];
+let subject = ["Clown ", "Police ", "Girl ", "Bear ", "Dog "];
+let action = ["stole my ", "took my ", "ate my ", "break my "];
+let posetion = ["car ", "keys ", "money ", "underwear ", "homework "];
+let where = ["on the beach. ", "in my house. ", "in my car. "];
+
+let excuse =
+  pronoun[Math.floor(Math.random() * pronoun.length)] +
+  subject[Math.floor(Math.random() * subject.length)] +
+  action[Math.floor(Math.random() * action.length)] +
+  posetion[Math.floor(Math.random() * posetion.length)];
+where[Math.floor(Math.random() * where.length)];
+
+console.log(excuse);
+
+let pe = document.querySelector("#excuse");
+pe.innerHTML = excuse;
